a7c6c40a0d5a136b7f61eabbad983fc8b1905afe
vbence86 <vbence86@gmail.com>
2020-05-26 10:43:31 +0200

    polish template code snippets

--- src/main.stories.js
+++ src/main.stories.js
@@ -5,6 +5,8 @@ import Component from './main.svelte';
 export default { title: 'Web Component' };
 
 export const withBetonlineTheme = () => {
+  // you can manually change the global theme inside
+  // the Storybook instance
   loadThemeGlobally('bol.css');
   return {
     Component,
@@ -15,6 +17,8 @@ export const withBetonlineTheme = () => {
 };
 
 export const withWildTheme = () => {
+  // you can manually change the global theme inside
+  // the Storybook instance
   loadThemeGlobally('wild.css');
   return {
     Component,
--- src/main.svelte
+++ src/main.svelte
@@ -1,32 +1,46 @@
 <script>
+  
   // @TODO Import your components here
   // ...
   import NestedElement from './components/NestedElement';
 
   /**
-   * Represents the name property of the web component
+   * Represents the "name" attribute of the web component
    *
    * @type {string}
    */
   export let name = 'World';
 
+
+  /**
+   * Private variable.
+   *
+   * @type {string}
+   */
+  const private = 'cannot see from the outside world';
+
   // @TODO add any public properties here through which the web component can communicate with the outside world
+
 </script>
 
 <style>
+
+  /* @TODO Remove this preset style */
   .hello-world-container {
     height: 5rem;
     margin: auto;
     width: 25rem;
   }
 
+  /* @TODO Remove this preset style */
   p {
     color: #00F;
     font-size: 1.5rem;
     text-shadow: 0 0 0.85rem rgba(0, 0, 0, .5);
   }
 
-  /* @TODO add your SASS style definitions here */  
+  /* @TODO Add your style definitions here */  
+
 </style>
 
 <!-- @TODO put the markup of the web component here -->
@@ -34,7 +48,7 @@
 
   <p>Hello {name}!</p>
 
-  <!-- @TODO you can break the application logic into child components-->
+  <!-- @TODO you can split the application logic into child components to follow the seperation of concerns methodology -->
   <NestedElement />
 
 </div>
